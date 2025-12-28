f13efc1eca9fa20ba501adfb269603456af6625f
vinay kumar <vinaycsc9@gmail.com>
2025-12-28 15:26:16 +0530

    Third commit

--- userside/src/App.jsx
+++ userside/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './componets/NavBar'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import { Home } from './pages/home'
+import { Home } from './pages/Home'
 import Footer from './componets/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
